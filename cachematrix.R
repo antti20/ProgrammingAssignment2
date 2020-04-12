c5e5bb431a80dc5fa0750d24f5d93322b5ca0302
antti20 <63555979+antti20@users.noreply.github.com>
2020-04-12 21:23:36 +0300

    Update cachematrix.R
    The two functions create a matrix and an inverse of that matrix. The one that calculates the inverse first checks if it has been calculated and if so retrieves it without calculating it again.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,46 @@
 ## functions do
 
 ## Write a short comment describing this function
+# This function creates a list of functions related to the matrix
+# that it takes as an input. This list contains:
+# 1) a function get that returns the matrix itself, 
+# 2) a function set that can be used to alter said matrix 
+# 3) a function setinverse, that is used in the cacheSolve function 
+# to create the inverse of the input function
+# 4) a function getinverse that returns the inverse of the matrix
+# (if it has been solved)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inv <<- solve
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse) 
 }
 
 
 ## Write a short comment describing this function
-
+# This function returns the inverse of a matrix that has been
+# used as an input in the makeCacheMatrix function. 
+# It also stores the inverted function in the list created
+# with the makeCacheMatrix
+# This function also checks if the inverted matrix has already been
+# created. If that is the case it returns from the list and does
+# not redo the calculation.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
